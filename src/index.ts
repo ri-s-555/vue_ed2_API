2c1cfe9a8da3418544c2284b2fb25f9ee456350f
ri_s <henviar39@gmail.com>
2025-05-08 11:34:53 +0300

    вариант корзины 3

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { MOCK_PRODUCTS } from './controlers/product'
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
   res.status(200)
+  
 })
 app.get('/products', (req: Request, res: Response) => {
   res.send(MOCK_PRODUCTS)
@@ -16,9 +17,30 @@ app.get('/products/:id', (req: Request, res: Response) => {
   const product = MOCK_PRODUCTS.find((product) => product.id === parseInt(req.params.id))
   res.send(product)
   res.status(200)
-  
 })
-app.post('')
+
+// app.get('/products/:id', (req: Request, res: Response) => {
+//   const product = MOCK_PRODUCTS.find((product) => product.id === parseInt(req.params.id));
+//   if (product) {
+//     res.status(200).send(product);
+//   } else {
+//     res.status(404).send('Product not found');
+//   }
+// });
+
+app.get('/cart', (req: Request, res: Response) => {
+  // Здесь должна быть логика получения данных корзины из базы данных или другого хранилища
+  const cart = getCartFromStorage(); // Предположим, что у вас есть такая функция
+  res.status(200).send(cart);
+});
+
+
+
+// app.post('')
+app.post('/products/:id', (req: Request, res: Response) => {
+  res.send('Product added to cart');
+});
+
 
 
 
